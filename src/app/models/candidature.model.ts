b763e54282b7e859749a487a473aebb9f15d8fc0
Louise <72506187+louisebld@users.noreply.github.com>
2023-03-12 20:41:35 +0100

    feat: candidature model + service

---
+++ src/app/models/candidature.model.ts
@@ -0,0 +1,11 @@
+export class Candidature {
+    constructor(
+        public id: number,
+        public name: string,
+        public fname: string,
+        public email: string,
+        public id_offre: number,
+        
+    ){
+    }
+}
---
+++ src/app/services/candidatures.services.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Candidature } from '../models/candidature.model';
+
+
+@Injectable ({
+    providedIn: 'root'
+})
+
+export class CandidaturesService {
+
+    // constructor() { }
+    constructor(private http: HttpClient) { }
+
+    createCandidature(candidature: Candidature): Observable<Candidature> {
+        console.log("Candidature :: " + candidature)
+        return this.http.post<Candidature>('http://localhost:8080/profilsearch/candidature/create', candidature);
+    }
+
+}
