be9e604297a1085f649f3d856c8af66076f2b3b2
Louise <72506187+louisebld@users.noreply.github.com>
2023-03-13 17:20:35 +0100

    question model & service

---
+++ src/app/services/questions.services.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Question } from '../models/question.model';
+
+@Injectable ({
+    providedIn: 'root'
+})
+
+export class QuestionsService {
+
+    // constructor() { }
+    constructor(private http: HttpClient) { }
+
+    getAllQuestions(): Observable<Question[]> {
+        return this.http.get<Question[]>('http://localhost:8080/profilsearch/question/all');
+    }
+
+
+
+}
