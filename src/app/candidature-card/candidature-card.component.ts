226eeabdc1861184aef3ccc5d32f73849d5ef558
Louise <72506187+louisebld@users.noreply.github.com>
2023-03-14 18:18:46 +0100

    feat: candidature card

---
+++ src/app/candidature-card/candidature-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Candidature } from '../models/candidature.model';
+
+@Component({
+  selector: 'app-candidature-card',
+  templateUrl: './candidature-card.component.html',
+  styleUrls: ['./candidature-card.component.scss']
+})
+export class CandidatureCardComponent implements OnInit {
+
+    @Input() candidature!: Candidature;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
