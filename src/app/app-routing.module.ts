3fdfc9ad8e98e27cc94f9d3937376162fd4a8c80
Louise <72506187+louisebld@users.noreply.github.com>
2023-03-16 19:46:38 +0100

    feat: add save data in local storage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ import { CreateOffreComponent } from './Component-Admin/create-offre/create-offr
 import { CandidatureCardComponent } from './Component-Admin/candidature-card/candidature-card.component';
 import { OffreAdminComponent } from './Component-Admin/offre-admin/offre-admin.component';
 import { PageOffreAdminComponent } from './Component-Admin/page-offre-admin/page-offre-admin.component';
-import { DeleteCandidatureComponent } from './delete-candidature/delete-candidature.component';
+import { DeleteCandidatureComponent } from './Component-User/delete-candidature/delete-candidature.component';
+import { OAuth2RedirectHandlerComponent } from './Component-Admin/oauth2-redirect-handler-component/oauth2-redirect-handler-component.component';
 
 const routes: Routes = [
   { 
@@ -43,6 +44,10 @@ const routes: Routes = [
   {
     path: 'delete/:id',
     component : DeleteCandidatureComponent,
+  },
+  {
+    path: 'oauth2/redirect',
+    component : OAuth2RedirectHandlerComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import {MatDialogModule} from '@angular/material/dialog';
+import { SocialLoginModule, SocialAuthServiceConfig, GoogleLoginProvider } from 'angularx-social-login';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,8 +21,11 @@ import { OffreAdminComponent } from './Component-Admin/offre-admin/offre-admin.c
 import { CandidatureCardComponent } from './Component-Admin/candidature-card/candidature-card.component';
 import { PageOffreAdminComponent } from './Component-Admin/page-offre-admin/page-offre-admin.component';
 import { ModalCandidatureComponent } from './Component-Admin/modal-candidature/modal-candidature.component';
-import { DeleteCandidatureComponent } from './delete-candidature/delete-candidature.component';
-
+import { DeleteCandidatureComponent } from './Component-User/delete-candidature/delete-candidature.component';
+import { Constantes } from './constantes';
+import { LocalService } from './services/local.service';
+import { AuthService } from './services/auth.services';
+import { OAuth2RedirectHandlerComponent } from './Component-Admin/oauth2-redirect-handler-component/oauth2-redirect-handler-component.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -36,7 +41,8 @@ import { DeleteCandidatureComponent } from './delete-candidature/delete-candidat
     CandidatureCardComponent,
     PageOffreAdminComponent,
     ModalCandidatureComponent,
-    DeleteCandidatureComponent
+    DeleteCandidatureComponent,
+    OAuth2RedirectHandlerComponent
   ],
   imports: [
     BrowserModule,
@@ -44,9 +50,28 @@ import { DeleteCandidatureComponent } from './delete-candidature/delete-candidat
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
-    MatDialogModule
+    MatDialogModule,
+    SocialLoginModule,
+    MatIconModule
   ],
-  providers: [],
+  providers: [
+    Constantes,
+    AuthService,
+    LocalService
+
+    // {
+    //   provide: 'SocialAuthServiceConfig',
+    //   useValue: {
+    //     autoLogin: false,
+    //     providers: [
+    //       {
+    //         id: GoogleLoginProvider.PROVIDER_ID,
+    //         provider: new GoogleLoginProvider('610269652409-q16dc1sskf4p1jvbk27msfsah035fbpl.apps.googleusercontent.com')
+    //       }
+    //     ]
+    //   } as SocialAuthServiceConfig,
+    // }
+  ],
   bootstrap: [AppComponent]
 })
 
