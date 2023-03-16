ac52529bf0c9117fe27a9312f12c6351d6711d18
Louise <72506187+louisebld@users.noreply.github.com>
2023-03-16 19:02:19 +0100

    feat: constantes file

---
+++ src/app/constantes.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Constantes {
+    API_BASE_URL = 'http://localhost:8080';
+    ACCESS_TOKEN = 'accessToken';
+    OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect';
+    GOOGLE_AUTH_URL ='http://localhost:8080' + '/oauth2/authorize/google?redirect_uri=' + 'http://localhost:3000/oauth2/redirect'
+    
+}
