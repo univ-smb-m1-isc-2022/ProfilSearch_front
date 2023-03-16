0320492e43eb7c51f6ae288d252f8062105b6728
Louise <72506187+louisebld@users.noreply.github.com>
2023-03-16 19:45:41 +0100

    feat: name in navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalService } from 'src/app/services/local.service';
+import { AuthService } from '../services/auth.services';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public localService: LocalService, private authService: AuthService) { }
+
+  name: string = '';
+  email: string = '';
+  imageUrl: string = '';
+  
+
 
   ngOnInit(): void {
+    if (this.localService.getData('accessToken')) {
+      this.name = this.localService.getData('name') as string;
+      this.email = this.localService.getData('email') as string;
+      this.imageUrl = this.localService.getData('imageUrl') as string;
+    }
+  }
+
+  logout() {
+    console.log("logout")
+    this.authService.logout();
   }
 
 }
