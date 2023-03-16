fa1e10f92263cd4326c7fd2499735c89963d27c4
Louise <72506187+louisebld@users.noreply.github.com>
2023-03-16 19:47:25 +0100

    refacto: dashboard admin component

--- src/app/Component-Admin/dashboard-admin/dashboard-admin.component.ts
+++ src/app/Component-Admin/dashboard-admin/dashboard-admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { Offre } from 'src/app/models/offre.model';
+import { LocalService } from 'src/app/services/local.service';
 import { OffresService } from 'src/app/services/offres.services';
 
 @Component({
@@ -12,11 +13,21 @@ export class DashboardAdminComponent implements OnInit {
 
   offres$!: Observable<Offre[]>;
 
-  constructor(private offresService : OffresService) { }
+  name: string = '';
+  email: string = '';
+  imageUrl: string = '';
+  
+
+  constructor(private offresService: OffresService, private localService: LocalService) { }
 
   ngOnInit(): void {
 
     this.offres$ = this.offresService.getAllOffres();
+    if (this.localService.getData('accessToken')) {
+      this.name = this.localService.getData('name') as string;
+      this.email = this.localService.getData('email') as string;
+      this.imageUrl = this.localService.getData('imageUrl') as string;
+    }
   }
 
 }
