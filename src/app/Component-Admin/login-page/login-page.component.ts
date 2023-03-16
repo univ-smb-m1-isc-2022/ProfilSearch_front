6ad68c21be58c1d24fc6b7f95aaa6f7a2602c200
Louise <72506187+louisebld@users.noreply.github.com>
2023-03-16 19:47:09 +0100

    refacto: login page

--- src/app/Component-Admin/login-page/login-page.component.ts
+++ src/app/Component-Admin/login-page/login-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Constantes } from 'src/app/constantes';
+import { LocalService } from 'src/app/services/local.service';
+import { AuthService } from 'src/app/services/auth.services';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router, public constantes: Constantes, private localService: LocalService) {}
 
   ngOnInit(): void {
+    const token = this.router.parseUrl(this.router.url).queryParams['token'];
+    if (token) {
+      localStorage.setItem('accessToken', token);
+    }
+
+    console.log('token:', token)
+
+    this.authService.getUserInfo(token)
+
+    // redirection vers /admin si l'utilisateur est déjà connecté
+    if (this.localService.getData('accessToken') && this.localService.getData('accessToken') !== 'null') {
+      this.router.navigate(['/admin']);
+    }
   }
+
+  loginWithGoogle() {
+    this.authService.loginWithGoogle()
+      .subscribe(response => {
+        // Si la connexion réussit, vous pouvez rediriger l'utilisateur vers une page d'accueil ou une autre page de l'application.
+        // Par exemple :
+        window.location.href = '/home';
+      }, error => {
+        // Si la connexion échoue, vous pouvez afficher un message d'erreur à l'utilisateur.
+        // Par exemple :
+        console.log('Erreur lors de la connexion avec Google : ', error);
+      });
+  }
+
 
 }
