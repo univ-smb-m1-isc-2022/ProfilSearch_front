35d6af87fda600bc8e3d0638e856ff0031023fb9
Louise <louiisebld@gmail.com>
2023-03-17 14:37:58 +0100

    refacto: link google auth url

--- src/app/constantes.ts
+++ src/app/constantes.ts
@@ -5,7 +5,10 @@ export class Constantes {
     API_BASE_URL = 'http://localhost:8080';
     ACCESS_TOKEN = 'accessToken';
     OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect';
-    GOOGLE_AUTH_URL = 'http://localhost:8080' + '/oauth2/authorize/google?invite_link="yoyo"&redirect_uri=' + 'http://localhost:3000/oauth2/redirect'
+    GOOGLE_AUTH_URL ='http://localhost:8080' + '/oauth2/authorize/google?redirect_uri=' + 'http://localhost:3000/oauth2/redirect'
+
+    // GOOGLE_AUTH_URL ='http://localhost:8080' + '/oauth2/authorize/google?redirect_uri=' + 'http://localhost:3000/oauth2/redirect'
+    // GOOGLE_AUTH_URL = 'http://localhost:8080' + '/oauth2/authorize/google?redirect_uri=' + 'http://localhost:3000/oauth2/redirect'
     // GOOGLE_AUTH_URL = 'http://localhost:8080/oauth2/authorize/google?redirect_uri=http://localhost:3000/oauth2/redirect?invite_link=";
 
     
