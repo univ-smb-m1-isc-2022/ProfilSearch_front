5ab1c44ab9b9d3d02a18745917dcfbae99894f02
Louise <72506187+louisebld@users.noreply.github.com>
2023-03-19 08:44:14 +0100

    feat: display image in page offre

--- src/app/Component-User/page-offre/page-offre.component.ts
+++ src/app/Component-User/page-offre/page-offre.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { Offre } from '../../models/offre.model';
 import { Reponse } from '../../models/reponse.model';
 import { ActivatedRoute } from '@angular/router';
@@ -10,11 +10,20 @@ import { CandidaturesService } from '../../services/candidatures.services';
   templateUrl: './page-offre.component.html',
   styleUrls: ['./page-offre.component.scss']
 })
-export class PageOffreComponent implements OnInit {
+export class PageOffreComponent implements OnInit, AfterViewInit {
 
   @Input () offre!: Offre;
+  offreImageURL!: string;
+  imageFile = new Blob();
+  imageUrl = '';
 
   constructor(private route: ActivatedRoute, private offresService: OffresService, private candidaturesService: CandidaturesService) { }
+  ngAfterViewInit(): void {
+    const imgElement = document.getElementById('offre-image') as HTMLImageElement;
+        console.log(imgElement)
+        if (imgElement)
+          imgElement.src = this.imageUrl;
+  }
 
   candidature: Candidature = new Candidature(0, '', '', '', { id: 0 }, []);
 
@@ -29,10 +38,22 @@ export class PageOffreComponent implements OnInit {
         for (let i = 0; i < this.offre.questions.length; i++) {
           this.candidature.reponses.push(new Reponse(0, '', this.offre.questions[i]));
         }
+        this.imageFile = this.base64ToBlob(this.offre.image.toString());
+        this.imageUrl = URL.createObjectURL(this.imageFile);
+
+      //   const imgElement = document.getElementById('offre-image') as HTMLImageElement;
+      //   console.log(imgElement)
+      //   if (imgElement)
+      //     imgElement.src = this.imageUrl;
       })
+
     }
+    
+    this.ngAfterViewInit();
+    
 
   }
+
   
   scrollToElement(elementId: string): void {
     const element = document.getElementById(elementId);
@@ -50,4 +71,35 @@ export class PageOffreComponent implements OnInit {
     })
 
   }
+
+
+getOffreImageURL(): string {
+  if (this.offre && this.offre.image) {
+    if (!this.offreImageURL) {
+      if (Array.isArray(this.offre.image)) {
+        const base64String = btoa(String.fromCharCode.apply(null, Array.from(this.offre.image)));
+        this.offreImageURL = 'data:image/jpeg;base64,' + base64String;
+      } else {
+        console.error('La propriété "image" de l\'offre doit être un tableau d\'entiers.');
+      }
+    }
+    return this.offreImageURL;
+  }
+  return '';
+  }
+  
+
+base64ToBlob(base64: string): Blob {
+  const binaryString = atob(base64);
+  const array = [];
+  for (let i = 0; i < binaryString.length; i++) {
+    array.push(binaryString.charCodeAt(i));
+  }
+  return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
+  }
+  
+  createImageUrlFromBlob(blob: Blob): string {
+  return URL.createObjectURL(blob);
+}
+
 }
