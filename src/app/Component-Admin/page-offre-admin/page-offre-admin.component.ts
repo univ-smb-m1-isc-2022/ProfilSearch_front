5b6df1b9ddd08e2c1e8b00aa0184b14bda4b04dc
Louise <louiisebld@gmail.com>
2023-04-01 12:09:12 +0200

    refacto: image in page offre admin

--- src/app/Component-Admin/page-offre-admin/page-offre-admin.component.ts
+++ src/app/Component-Admin/page-offre-admin/page-offre-admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { Offre } from '../../models/offre.model';
 import { Reponse } from '../../models/reponse.model';
 import { ActivatedRoute } from '@angular/router';
@@ -12,13 +12,25 @@ import { ModalCandidatureComponent } from '../modal-candidature/modal-candidatur
   templateUrl: './page-offre-admin.component.html',
   styleUrls: ['./page-offre-admin.component.scss']
 })
-export class PageOffreAdminComponent implements OnInit {
+export class PageOffreAdminComponent implements OnInit, AfterViewInit {
 
 
   @Input () offre!: Offre;
+  offreImageURL!: string;
+  imageFile = new Blob();
+  imageUrl = '';
 
   constructor(private route: ActivatedRoute, private offresService: OffresService, private candidaturesService: CandidaturesService, private dialog: MatDialog) { }
-
+  
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit")
+    const imgElement = document.getElementById('offre-image') as HTMLImageElement;
+    console.log(imgElement)
+    if (imgElement)
+      imgElement.src = this.imageUrl;
+    // refresh the view
+    imgElement.src = this.imageUrl;
+  }
   candidatures!: Candidature[];
 
   ngOnInit(): void {
@@ -28,6 +40,28 @@ export class PageOffreAdminComponent implements OnInit {
         this.offre = offre;
         console.log("Offre :: " + offre)
         console.log("Questions :: " + offre.questions.at(0))
+
+        const imgElement = document.getElementById('offre-image') as HTMLImageElement;
+
+        if (imgElement && this.offre.image) {
+          imgElement.src = this.offre.imageUrl;
+          const blob = new Blob([new Uint8Array(this.offre.image)], { type: 'image/jpeg' });
+          const imageUrl = URL.createObjectURL(blob);
+
+          this.offre.imageUrl = imageUrl;
+          imgElement.src = this.offre.imageUrl;
+
+          this.imageFile = this.base64ToBlob(this.offre.image.toString());
+          this.imageUrl = URL.createObjectURL(this.imageFile);
+
+
+        } else {
+          var div = document.getElementById('div-image');
+          if (div)
+            div.style.display = 'none';
+        }
+
+
       })
 
       this.candidaturesService.getCandidaturesByOffreId(id).subscribe((candidatures) => {
@@ -61,6 +95,20 @@ export class PageOffreAdminComponent implements OnInit {
       console.log(`Dialog result: ${result}`);
     });
   }
+
+  base64ToBlob(base64: string): Blob {
+  const binaryString = atob(base64);
+  const array = [];
+  for (let i = 0; i < binaryString.length; i++) {
+    array.push(binaryString.charCodeAt(i));
+  }
+  return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
+  }
+  
+  createImageUrlFromBlob(blob: Blob): string {
+  return URL.createObjectURL(blob);
+}
+
 
 
 }
